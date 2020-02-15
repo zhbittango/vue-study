9dc163d3c2bd3cc822b159ff67d904d1de11637c
zhbittango <2468993759@qq.com>
2020-02-15 23:46:45 +0800

    webpack初体验

---
+++ day4/12-前端模块化/03-webpack初体验/src/info.js
@@ -0,0 +1,3 @@
+// es6 导出
+export var name = 'tango';
+export var age = '25';
---
+++ day4/12-前端模块化/03-webpack初体验/src/main.js
@@ -0,0 +1,12 @@
+
+// commonjs 导入
+const {add, mul} = require('./mathUtil');
+
+console.log(add(20, 30));
+console.log(mul(20, 30));
+
+// es6 导入
+import {name, age} from "./info";
+console.log(name);
+console.log(age);
+
