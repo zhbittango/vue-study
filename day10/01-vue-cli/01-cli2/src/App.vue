f4e75e1ccc67d8d4421231f2a87f08bc7383af90
zhbittango <2468993759@qq.com>
2020-02-22 23:38:42 +0800

    脚手架、箭头函数

---
+++ day10/01-vue-cli/01-cli2/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <h3>
+      npm install -g @vue/cli; 安装vue脚手架
+      <br>
+      npm install -g @vue/cli-init; 拉取 2.x 模板 (旧版本)
+      <br>
+      vue init webpack 项目名称; 脚手架2创建项目
+      <br>
+    </h3>
+    <img src="./assets/logo.png">
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
