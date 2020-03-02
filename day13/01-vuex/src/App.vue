48323e998c7efcbd3bc3e5c1f45fe42a6d684385
zhbittango <2468993759@qq.com>
2020-03-03 01:14:01 +0800

    store、getters、mutations使用

--- day13/01-vuex/src/App.vue
+++ day13/01-vuex/src/App.vue
@@ -1,8 +1,26 @@
 <template>
-  <div id="app">
+  <div id="app"> 
+    <h2>{{$store.state.info}}</h2>
+    <button @click="mutation">mutation响应式</button>
     <h2>{{$store.state.counter}}</h2>
-    <button @click="$store.state.counter++">+</button>
-    <button @click="$store.state.counter--">-</button>
+
+    <button @click="add">+</button>
+    <button @click="reduce">-</button>
+    <!-- mutations传参 -->
+    <button @click="addCount(5)">+5</button>
+    <button @click="addCount(10)">+10</button>
+    <button @click="addStu">添加学生</button>
+    <button @click="mul">mul</button>
+    <!-- <button @click="mulall">mulall</button> -->
+    
+
+    <!-- 相当于computed -->
+    <h2>{{$store.getters.power}}</h2>
+    <h2>{{$store.getters.more20}}</h2>
+    <h2>{{$store.getters.more20len}}</h2>
+    <!-- 返回值为function时可以传参 -->
+    <h2>{{$store.getters.input(10)}}</h2>
+
     <hello-vuex :counter='counter'></hello-vuex>
   </div>
 </template>
@@ -23,10 +41,28 @@ export default {
   },
   methods: {
     add() {
-      this.counter ++
+      // this.counter ++
+      this.$store.commit('add') // 提交mutation
     },
     reduce() {
-      this.counter --
+      // this.counter --
+      this.$store.commit('reduce')  // 提交mutation
+    },
+    addCount(num) {
+      this.$store.commit('addCount', num)
+    },
+    addStu() {
+      const stu = {id: 5, name: 'ee', age: 30}
+      this.$store.commit('addStu', stu)
+    },
+    mul() {
+      this.$store.commit({
+        type: 'mul1',
+        count: 2
+      })
+    },
+    mutation() {
+      this.$store.commit('mutation')
     }
   },
 }
--- day13/01-vuex/src/store/index.js
+++ day13/01-vuex/src/store/index.js
@@ -5,21 +5,69 @@ Vue.use(Vuex)
 
 const stoge = new Vuex.Store({
   state: {
-    counter: 0
+    counter: 0,
+    students: [
+      {id: 1, name: 'aa', age: 25},
+      {id: 2, name: 'bb', age: 12},
+      {id: 3, name: 'cc', age: 18},
+      {id: 4, name: 'dd', age: 5},
+    ],
+    info: {
+      name: 'tango',
+      age: 25,
+      hobby: 'coding'
+    }
   },
   mutations: {
-    add() {
+    add(state) {
       state.counter ++;
     },
-    reduce() {
+    reduce(state) {
       state.counter --;
+    },
+    addCount(state, num) {
+      state.counter += num
+    },
+    addStu(state, stu) {
+      state.students.push(stu);
+    },
+    mul1(state, payload) {
+      console.log(payload);
+      state.counter *= payload.count
+    },
+    mul2(state, payload) {
+      console.log(payload);
+      // state.counter *= payload.count
+    },
+    mutation(state) {
+      // this.state.info.name = 'ngota' // 响应式
+      // this.state.info['like'] = '1212'; // 单独修改不会响应式改变，如果有其他响应式改变，此时可生效
+      // Vue.set(state.info, 'like', 'ngota') // 响应式
+      // delete state.info.name // 不是响应式
+      Vue.delete(state.info, 'name') // 响应式
+
     }
   },
   actions: {
 
   },
+  // getters的使用
   getters: {
-
+    power(state) {
+      return state.counter * state.counter
+    },
+    more20(state) {
+      // return state.student.filter(function(n){
+      //   return n.age > 20
+      // })
+      return state.students.filter(n => n.age > 20)
+    },
+    more20len(state, getters) {
+      return getters.more20.length
+    },
+    input(state) {
+      return age => state.students.filter(n => n.age > age)
+    }
   },
   modules: {
 
