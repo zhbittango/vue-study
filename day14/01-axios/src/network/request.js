e70887a6dc610b1cc83f6700b6d1f5dcca3a81ae
zhbittango <2468993759@qq.com>
2020-03-05 15:35:16 +0800

    axios拦截器

--- day14/01-axios/src/network/request.js
+++ day14/01-axios/src/network/request.js
@@ -6,8 +6,32 @@ export function request(config) {
       baseURL: "http://123.207.32.32:8000",
       timeout: 5000
     });
+
+    // 请求拦截器
+    instance.interceptors.request.use(config => {
+      // 1.config中一些信息不符合服务器的要求
+
+      // 2.发送网络请求时，希望在界面显示一个请求的图标
+
+      // 3.某些网络请求(比如登录token)，必须携带一些特殊信息
+      return config;
+    }, err => {
+      return err
+    })
+
+    // 响应拦截器
+    instance.interceptors.response.use(res => {
+      // 过滤某些响应信息
+      return res.data
+    }, err => {
+      return err
+    })
+    
     return instance(config)
 }
+
+
+
 
 // 3
 // export function request(config) {
