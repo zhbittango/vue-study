bea16eff5cfdadd69e3d79f7e2861b76870a6ee2
zhbittango <2468993759@qq.com>
2020-03-06 18:01:38 +0800

    home.js封装请求

--- day15/mall/vue.config.js
+++ day15/mall/vue.config.js
@@ -6,6 +6,7 @@ module.exports = {
       alias: {
         "components": '@/components',
         "content": 'components/content',
+        "common": 'components/common',
         "assets": '@/assets',
         "network": '@/network',
         "views": '@/views',
